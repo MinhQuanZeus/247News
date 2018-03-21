0df3fd7a40a49c282a9989bf27821f5acea34edc
QuanTM9 <quantm9@fsoft.com.vn>
2018-03-22 01:30:15 +0700

    progress bar webview

--- app/src/main/java/zeus/quantm/a247news/activities/ReaderActivity.java
+++ app/src/main/java/zeus/quantm/a247news/activities/ReaderActivity.java
@@ -1,15 +1,18 @@
 package zeus.quantm.a247news.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.support.v7.widget.Toolbar;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import zeus.quantm.a247news.R;
@@ -21,10 +24,15 @@ public class ReaderActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private WebView webView;
     private New newModel;
+    private ProgressBar progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reader);
+        progress = (ProgressBar) findViewById(R.id.progressBar);
+        progress.setMax(100);
+        progress.getProgressDrawable().setColorFilter(
+                Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
         setSupportActionBar(toolbar);
@@ -36,14 +44,19 @@ public class ReaderActivity extends AppCompatActivity {
         });
         webView = (WebView)findViewById(R.id.wv_reader);
         webView.setWebViewClient(new MyBrowser());
+        webView.setWebChromeClient(new MyWebViewClient());
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
         newModel = (New)bundle.getSerializable("newModel");
         Log.d("Reader", newModel.toString());
         getSupportActionBar().setTitle(newModel.title);
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(newModel.link);
     }
 
+    public void setValue(int progress) {
+        this.progress.setProgress(progress);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.reader_menu, menu);
@@ -68,6 +81,14 @@ public class ReaderActivity extends AppCompatActivity {
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
+        }
+    }
+
+    private class MyWebViewClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            ReaderActivity.this.setValue(newProgress);
+            super.onProgressChanged(view, newProgress);
         }
     }
 }
